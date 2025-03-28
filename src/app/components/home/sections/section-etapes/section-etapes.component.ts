3644dbe9d86f5946b93c84ca9dcb0175d4a97e0a
NabilKADOURI <kadourinabil7@gmail.com>
2025-03-28 12:08:05 +0100

    feat(home):added component section-etapes and its content

--- src/app/components/home/sections/section-etapes/section-etapes.component.ts
+++ src/app/components/home/sections/section-etapes/section-etapes.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Inject, inject } from '@angular/core';
+import { LIST_ETAPES } from '../../../../shared/data';
+
 
 @Component({
   selector: 'app-section-etapes',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './section-etapes.component.html',
   styleUrl: './section-etapes.component.css'
 })
 export class SectionEtapesComponent {
-
+  etapes = LIST_ETAPES;
 }
