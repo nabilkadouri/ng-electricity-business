518f1c8a7542a54b6afbdd68f0f4aefe06534b33
NabilKADOURI <kadourinabil7@gmail.com>
2025-03-28 12:09:26 +0100

    feat(home):added component section-testimonial and its content

---
+++ src/app/components/home/sections/section-testimonial/section-testimonial.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-testimonial',
+  imports: [],
+  templateUrl: './section-testimonial.component.html',
+  styleUrl: './section-testimonial.component.css'
+})
+export class SectionTestimonialComponent {
+
+}
