f24661a7896f23a6edc3282052589f839486aa1f
NabilKADOURI <kadourinabil7@gmail.com>
2025-03-28 12:08:35 +0100

    feat(home):added component section service and its content

---
+++ src/app/components/home/sections/section-services/section-services.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-services',
+  imports: [],
+  templateUrl: './section-services.component.html',
+  styleUrl: './section-services.component.css'
+})
+export class SectionServicesComponent {
+
+}
