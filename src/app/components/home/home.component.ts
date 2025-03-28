292a0c687e35a40618978009ef67a0b9ad7f2879
NabilKADOURI <kadourinabil7@gmail.com>
2025-03-28 12:05:45 +0100

    feat : added composant home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './sections/hero/hero.component';
 import { SectionEtapesComponent } from './sections/section-etapes/section-etapes.component';
+import { SectionServicesComponent } from './sections/section-services/section-services.component';
+import { SectionSimulatorComponent } from './sections/section-simulator/section-simulator.component';
+import { SectionTestimonialComponent } from './sections/section-testimonial/section-testimonial.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent,SectionEtapesComponent],
+  imports: [HeroComponent,SectionEtapesComponent,SectionServicesComponent,SectionSimulatorComponent,SectionTestimonialComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
