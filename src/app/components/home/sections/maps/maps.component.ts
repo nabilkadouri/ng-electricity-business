86b3aedcc669233b2ef9ed311c2872ddb39acb24
NabilKADOURI <kadourinabil7@gmail.com>
2025-04-05 10:30:34 +0200

    add component map

---
+++ src/app/components/home/sections/maps/maps.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component} from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-maps',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: `maps.component.html`
+})
+export class MapsComponent {
+  
+}
