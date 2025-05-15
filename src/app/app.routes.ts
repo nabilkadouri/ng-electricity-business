9c809cef98519669726b4339be4e6ee55968a50e
Nabil Kadouri <kadourinabil7@gmail.com>
2025-05-15 09:16:55 +0200

    feat: Add navigation routes for authentication pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { CheckCodeComponent } from './components/check-code/check-code.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RegisterFormComponent } from './components/register-form/register-form.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent},
+  { path: 'login', component: LoginFormComponent},
+  { path: 'check', component: CheckCodeComponent },
+  { path: 'register', component: RegisterFormComponent },
+  { path: 'dashboard',  component: DashboardComponent}
 ];
