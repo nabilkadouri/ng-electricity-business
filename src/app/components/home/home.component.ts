21d9aa706a522d6fcd581de4fc70ed4b3fd64c3a
Nabil Kadouri <kadourinabil7@gmail.com>
2025-05-16 11:58:43 +0200

    feat(home): Update general styling and sizes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,14 +3,13 @@ import { HeroComponent } from './sections/hero/hero.component';
 
 import { SectionSimulatorComponent } from './sections/section-simulator/section-simulator.component';
 import { SectionTestimonialComponent } from './sections/section-testimonial/section-testimonial.component';
-import { SectionReserveChargerComponent } from "./sections/section-reserve-charger/section-reserve-charger.component";
 import { SectionProposeChargerComponent } from "./sections/section-propose-charger/section-propose-charger.component";
 import { SectionSimpleProcessComponent } from "./sections/section-simple-process/section-simple-process.component";
 import { MapsComponent } from "./sections/maps/maps.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, SectionSimulatorComponent, SectionTestimonialComponent, SectionReserveChargerComponent, SectionProposeChargerComponent, SectionSimpleProcessComponent, MapsComponent],
+  imports: [HeroComponent, SectionSimulatorComponent, SectionTestimonialComponent, SectionProposeChargerComponent, SectionSimpleProcessComponent, MapsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
