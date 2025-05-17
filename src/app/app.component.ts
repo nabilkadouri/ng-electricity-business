18a76d8c457a950c371dbb4230e7fbae3bfecb0a
Nabil Kadouri <kadourinabil7@gmail.com>
2025-05-17 12:08:55 +0200

    feat(routing): Define routes for public and dashboard sections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import {initFlowbite} from 'flowbite'
+import { HeaderComponent } from './layouts/public-layout/components/header/header.component';
+import { FooterComponent } from './layouts/public-layout/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent,FooterComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
