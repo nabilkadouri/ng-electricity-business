51bbdd6f199ded255f1abb3e1e9c7fc20aee9744
Nabil Kadouri <kadourinabil7@gmail.com>
2026-02-05 16:25:18 +0100

    fix(env): use https pictureUrl in production to avoid mixed content

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://api.electricity-business.nk-dev.fr'
+    apiUrl: 'https://api.electricity-business.nk-dev.fr',
+    pictureUrl: 'https://api.electricity-business.nk-dev.fr/'
     
   };
